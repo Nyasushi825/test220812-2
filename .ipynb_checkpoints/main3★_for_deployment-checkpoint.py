0eb7e30eea820abb293a3fca6287a6c338391146
Yasushi Nomura <yasushi.nomura825@gmail.com>
2022-08-13 22:36:39 +0800

    1st commit

--- .ipynb_checkpoints/main3★_for_deployment-checkpoint.py
+++ .ipynb_checkpoints/main3★_for_deployment-checkpoint.py
@@ -1,10 +1,4 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import altair as alt
-from PIL import Image
-
 import time
 
 st.title('Streamlit Trial')
